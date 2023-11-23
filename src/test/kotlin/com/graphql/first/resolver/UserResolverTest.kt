b0845b16d5c9d69b83aea417eb3b615f32f43948
R0bert196 <robertchiriac196@yahoo.com>
2023-11-23 12:09:48 +0200

    create integration test for create user

---
+++ src/test/kotlin/com/graphql/first/resolver/UserResolverTest.kt
@@ -0,0 +1,88 @@
+package com.graphql.first.resolver
+
+import com.graphql.first.repositories.UserRepository
+import com.graphql.first.services.UserService
+import com.graphql.first.util.JwtUtil
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.ints.shouldBeBetween
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.graphql.test.tester.HttpGraphQlTester
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.reactive.server.WebTestClient
+import java.util.*
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class UserResolverTest(
+    @LocalServerPort val randomServerPort: Int,
+    @Value("\${secretKey}") val signedKey: String,
+    val userService: UserService
+) : DescribeSpec({
+
+    lateinit var graphQlTesterForUnsecureOperations: HttpGraphQlTester
+    lateinit var graphQlTesterForSecureOperations: HttpGraphQlTester
+
+    beforeSpec {
+        val client = WebTestClient.bindToServer()
+            .baseUrl("http://localhost:$randomServerPort/graphql")
+            .build()
+
+        graphQlTesterForUnsecureOperations = HttpGraphQlTester.create(client)
+
+        graphQlTesterForSecureOperations = graphQlTesterForUnsecureOperations
+            .mutate()
+            .headers {
+                it.set(
+                    "Authorization", JwtUtil.generateJwtToken(
+                        username = "John2",
+                        signedSecret = signedKey,
+                        roles = listOf("ROLE_USER")
+                    )
+                )
+            }.build()
+    }
+
+    describe("User resolver") {
+
+        it("should allow to create a new user") {
+
+            // language=GraphQL
+            val createUserMutation = """
+                mutation {
+                addUser(addUserInput: {name: "Vikas", password: "pass", roles: "ROLE_USER"})
+                }
+            """.trimIndent()
+
+            val users = userService.getUsers(0, 10000)
+            users.size.shouldBe(0)
+
+            val userId = graphQlTesterForUnsecureOperations.document(
+                createUserMutation
+            ).execute()
+                .path("addUser")
+                .entity(String::class.java)
+                .get()
+
+            val newUserList = userService.getUsers(0, 10000)
+
+            newUserList.size.shouldBe(1)
+
+            newUserList.firstOrNull {
+                it.id == UUID.fromString(userId)
+            }.shouldNotBeNull()
+                .name.shouldBe("Vikas")
+        }
+
+
+    }
+
+
+}) {
+    @Autowired
+    private lateinit var userRepository: UserRepository
+}
