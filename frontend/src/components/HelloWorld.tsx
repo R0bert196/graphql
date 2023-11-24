39f6b9feb7a92879d3d2dc0bbb7b88b7911ff630
R0bert196 <robertchiriac196@yahoo.com>
2023-11-24 11:08:38 +0200

    add codegen to generate backend schema

--- frontend/src/components/HelloWorld.tsx
+++ frontend/src/components/HelloWorld.tsx
@@ -1,13 +1,14 @@
 import { gql, useQuery } from "@apollo/client";
+import { HelloWorldDocument } from "../gql/graphql";
 
 const HELLO_WORLD_QUERY = gql`
-  query {
+  query  HelloWorld {
     helloWorld
   }
 `;
 
 const HelloWorld = () => {
-  const { data, error, loading } = useQuery(HELLO_WORLD_QUERY);
+  const { data, error, loading } = useQuery(HelloWorldDocument);
 
   return (
     <>
