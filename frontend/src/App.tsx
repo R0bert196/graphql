b4e8d33557755d44af446b5e43935892b221ae17
R0bert196 <robertchiriac196@yahoo.com>
2023-11-27 17:27:10 +0200

    implement require auth component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,19 +3,25 @@ import "./App.css";
 import HelloWorld from "./components/HelloWorld";
 import Layout from "./components/common/layout/Index";
 import LoginContainer from "./components/login/LoginContainer";
+import RequireAuth from "./components/common/RequireAuth";
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Layout />}>
         <Route index element={<div>Home Page</div>} />
-        <Route path='/posts'>
-          <Route index element={<div>Post page</div>} />
-          <Route path='new' element={<div>New Post</div>} />
+
+        <Route element={<RequireAuth />}>
+          <Route path='/posts'>
+            <Route index element={<div>Post page</div>} />
+            <Route path='new' element={<div>New Post</div>} />
+          </Route>
+          <Route path='/users' element={<div>Users Page</div>} />
+          <Route path='/comments' element={<div>Comments Page</div>} />
+          <Route path='/my-posts' element={<div>My posts Page</div>} />
         </Route>
-        <Route path='/users' element={<div>Users Page</div>} />
-        <Route path='/comments' element={<div>Comments Page</div>} />
-        <Route path='/my-posts' element={<div>My posts Page</div>} />
+
+        {/* public routes  */}
         <Route path='/login' element={<LoginContainer />} />
         <Route path='/register' element={<div>Register Page</div>} />
       </Route>
